97ef90d0a1249bd1a7d7c61b1cb86cfb7792dbfb
therakeshpurohit <rakeshpurohit909@gmail.com>
2020-06-30 12:51:39 +0530

    Data Fetching with useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import MultiCountReducers from './components/MultiCountReducers';
+import DataFetchingTwo from './components/DataFetchingTwo';
 
 export const UserContext = React.createContext()
 export const ChannelContext = React.createContext()
@@ -8,7 +8,8 @@ export const ChannelContext = React.createContext()
 function App() {
   return (
     <div className="App">
-      <MultiCountReducers />
+      <DataFetchingTwo />
+      {/* <MultiCountReducers /> */}
       {/* <CountReducerTwo /> */}
       {/* <UserContext.Provider value={'Rakesh'}>
                 <ChannelContext.Provider value={'The Big Bang Theory'}>
